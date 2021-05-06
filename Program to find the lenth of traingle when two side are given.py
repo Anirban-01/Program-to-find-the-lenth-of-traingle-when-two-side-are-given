a88356477a8a2e2048ff70120ede7887960280b7
Python learner <areyaban@gmail.com>
2021-05-06 22:12:16 +0530

    Add files via upload

---
+++ Program to find the lenth of traingle when two side are given.py
@@ -0,0 +1,15 @@
+# As an exercise, use incremental development to write a function called hypotenuse that returns the length of the
+# # hypotenuse of a right triangle given the lengths of two legs as arguments. Record each stage of the incremental
+# # development process as you go.
+
+import math
+
+
+def hypotenuse(perpendicular=5, base=5):
+    perpendicular
+    base
+    result = math.sqrt(perpendicular ** 2 + base ** 2)
+    return print(result)
+
+
+hypotenuse()
